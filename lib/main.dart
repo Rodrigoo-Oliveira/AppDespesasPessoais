58881670154f63a339f40abe77a3b41c0bda6570
Rodigo Oliveira <rodigoodeoliveira.santos@gmail.com>
2021-07-31 18:30:55 -0300

    Dividindo App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'components/transaction_user.dart';
+
+
 
 main() => runApp(ExpensesApp()); 
 
@@ -7,24 +9,13 @@ class ExpensesApp extends StatelessWidget {
 
 @override
 build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      home: MyHomePage(),
+    );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(id: 't1',
-      title: 'Tênis',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(id: 't2',
-      title: 'Conta de energia',
-      value: 211.30,
-      date: DateTime.now(),
-    )
-  ];
-
 
   @override
   Widget build(BuildContext context) {
@@ -33,47 +24,15 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             child: Card(
-              color: Colors.purple,
+              color: Colors.blue,
               child: Text('Gráfico'),
             ),
           ),
-          Column(
-            children: _transactions.map((tr){
-              return Card(
-                child:Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 2,
-                        )
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child:Text(
-                        tr.value.toString()
-                      ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text(tr.title),
-                        Text(tr.date.toString()),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          )
+          TransactionUser(),
         ],
       ),
     );
