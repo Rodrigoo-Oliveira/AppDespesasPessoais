d6e4c480ed657bf48a8dd1c9602029533090394f
Rodigo Oliveira <rodigoodeoliveira.santos@gmail.com>
2021-08-02 23:29:39 -0300

    Correção Formulário

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  
-  
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  final void Function(String, double) onSubmit; 
+class TransactionForm extends StatefulWidget {
+  final void Function(String, double) onSubmit;
 
   TransactionForm(this.onSubmit);
+
+  @override
+  _TransactionFormState createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+
+  final valueController = TextEditingController();
+
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+
+    widget.onSubmit(title, value);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +35,16 @@ class TransactionForm extends StatelessWidget {
           children: <Widget>[
             TextField(
               controller: titleController,
+              onSubmitted: (_) => _submitForm,
               decoration: InputDecoration(
                 labelText: 'Título',
               ),
             ),
             TextField(
-                controller: valueController,
-                decoration: InputDecoration(
+              controller: valueController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitForm,
+              decoration: InputDecoration(
                 labelText: 'Valor (R\$)',
               ),
             ),
@@ -38,17 +56,13 @@ class TransactionForm extends StatelessWidget {
                     'Nova Transação',
                     style: TextStyle(color: Colors.purple),
                   ),
-                  onPressed: () {
-                    final title = titleController.text;
-                    final value = double.tryParse(valueController.text) ?? 0.0;
-                    onSubmit(title, value);
-                  },
+                  onPressed: _submitForm,
                 )
               ],
             ),
           ],
         ),
-      )
+      ),
     );
   }
 }
