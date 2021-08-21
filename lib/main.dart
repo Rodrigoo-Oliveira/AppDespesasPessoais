7704c554fe77f050628543e94222c1bba2732d8c
Rodrigo Oliveira <rodrigoodeoliveira.santos@gmail.com>
2021-08-21 11:03:39 -0300

    Ajustando gráfico

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:expenses/components/chart.dart';
 import 'package:flutter/material.dart';
-
 import 'dart:math';
 import './components/transaction_form.dart';
 import './components/transaction_list.dart';
@@ -51,6 +50,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [];
 
+  bool _showChart = false;
+
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
       return tr.date.isAfter(DateTime.now().subtract(
@@ -91,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
+    bool isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text(
         'Despesas Pessoais',
@@ -115,14 +116,31 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              height: availableHeight * 0.30,
-              child: Chart(_recentTransactions),
-            ),
-            Container(
-              height: availableHeight * 0.70,
-              child: TransactionList(_transactions,_removeTransaction),
-            ),
+            if(isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:<Widget>[
+                  Text('Exibir Gráfico'),
+                  Switch(
+                    value: _showChart,
+                    onChanged: (value) {
+                      setState(() {
+                        _showChart = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            if(_showChart || !isLandscape) 
+                Container(
+                  height: availableHeight * (isLandscape ? 0.7 : 0.30),
+                  child: Chart(_recentTransactions),
+                ),
+            if(!_showChart || !isLandscape)  
+              Container(
+                height: availableHeight * 0.70,
+                child: TransactionList(_transactions,_removeTransaction),
+              ),
           ],
         ),
       ),
